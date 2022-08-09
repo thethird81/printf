0051ade3c242f42aacb5b7fd3ad60d3f50eb2dca
kingpascal <34338394+kingpascal@users.noreply.github.com>
2022-08-09 18:12:10 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,15 +1,20 @@
 #include "main.h"
-
 /************************* PRINT CHAR *************************/
-
 /**
  * print_char - Prints a char
+ *
  * @types: List a of arguments
+ *
  * @buffer: Buffer array to handle print
+ *
  * @flags:  Calculates active flags
+ *
  * @width: Width
+ *
  * @precision: Precision specification
+ *
  * @size: Size specifier
+ *
  * Return: Number of chars printed
  */
 int print_char(va_list types, char buffer[],
